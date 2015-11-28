037a42c5fbfb2fe63994e46648cd79b93ed7b6ff
mina37 <minasherif7@hotmail.com>
2015-11-28 23:02:15 +0200

    first trial web app

---
+++ DjangoWebProject3/DjangoWebProject3/app/models.py
@@ -0,0 +1,23 @@
+﻿"""
+Definition of models.
+"""
+
+from django.db import models
+from django.template.backends import django
+from django import forms
+from django.forms import ModelForm
+
+# Create your models here.
+class Artist(models.Model):
+    name = models.CharField("artist",max_length = 50)
+    year_formed = models.PositiveIntegerField()
+class Album(models.Model):
+    name = models.CharField("album",max_length = 50)
+    artist = models.ForeignKey(Artist)
+class users(models.Model):
+    uname=models.CharField("Username",max_length=50)
+    password=models.CharField("Password",max_length=50)
+class usersforms(forms.ModelForm):
+    class Meta:
+        model = users
+        fields = ['uname','password']
---
+++ DjangoWebProject3/DjangoWebProject3/app/views.py
@@ -0,0 +1,66 @@
+﻿"""
+Definition of views.
+"""
+
+from django.shortcuts import render,render_to_response
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+from django.http.response import HttpResponse, HttpResponseRedirect
+from app.models import *
+
+def user(request):
+    usrs=users.objects.all()
+    return render_to_response('app/list of users.html',{'usrs':usrs})
+
+
+def register(request):
+    if request.method=='GET':
+        form=usersforms();
+        return render(request, 'app/WebPage1.html',{'form':form})
+    elif request.method =='POST':
+        form=usersforms(request.POST)
+        form.save()
+        return HttpResponseRedirect('')
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        })
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        })
+    )
